8786d04902c885b1705bd19f9ad5632cba6c7000
Goldtiger997 <goltiger9971009@gmail.com>
2022-04-02 19:30:39 +0800

    Make icons and text update based on the day of the week

--- client/js/game.js
+++ client/js/game.js
@@ -1,13 +1,16 @@
 const rounds = 10;
+const nutrients = ["Energy","Fat","Fibre","Iron","Protein","Sodium","Sugar"];
+// This function is used to generate a new batch of icons each day, and also to change the nutrient text
 
-// This function is used to generate a new batch of icons each day
-// TODO: Pass in the correct icon for the nutrient of the day
 function generateRoundIcons() {
+  //TODO: Consider renaming this function as it does more than generate round icons
   let div = document.getElementById('round-icons');
+  let dt = new Date();
   for (let i = 0; i < rounds; i++) {
     let img = document.createElement('img');
-    img.src = '../images/energy-lose.png';
+    img.src = "../images/" + nutrients[dt.getDay()].toLowerCase() + "-lose.png";
     img.setAttribute('alt', 'roundIcon')
     div.appendChild(img);
   }
+  document.getElementById("nutrient-name-text").innerHTML = nutrients[dt.getDay()];
 }
