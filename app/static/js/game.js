a8e8cc36096148c1430f6eecf040c954c890b636
Goldtiger997 <goldtiger9971009@gmail.com>
2022-04-18 09:05:05 +0800

    First attempt at animation. Doesn't work for narrow screens where the foods are on top of each other

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -1,10 +1,16 @@
 const rounds = 10;
 let currentRound = 1;
 const nutrients = ["Calcium", "Fat", "Fibre", "Iron", "Protein", "Sodium", "Sugar"];
-const units = ["mg", "g", "g", "mg", "g", "mg", "g"]
+const units = ["kJ", "g", "g", "mg", "g", "mg", "g"]
+// TODO: Use the units from the nutrition database rather than hardcoded (the only part of each key that is in brackets)
 
 let dt = new Date();
 let todaysNutrient = nutrients[dt.getDay()];
+
+let start = -1; //If start is not -1, then an animation is ongoing, so clicking on the foods is disabled
+let previousTimeStamp;
+const slideTime = 500; //number of milliseconds for animation to take
+const slidePower = 0.4; //The sliding follows the function t^slidePower. Set this to 1 for linear sliding
 
 /**
  * Called when the page loads, sets up the game 
@@ -20,8 +26,8 @@ function onLoad() {
  */
 function generateFoodChoices() {
   for (var i = 0; i < 2; i++) {
-    document.getElementById("food-selection-area").children[i].style.backgroundImage = "url(" + Object.values(data)[currentRound-1+i].url + ")";
-    document.getElementsByClassName("food-name-text")[i].innerHTML = Object.keys(data)[currentRound-1+i]
+      document.getElementById("food-selection-area").children[i].style.backgroundImage = "url(" + Object.values(data)[currentRound-1+i].url + ")";
+      document.getElementsByClassName("food-name-text")[i].innerHTML = Object.keys(data)[currentRound-1+i]
   }
   document.getElementById("nutrient-data-text").innerHTML = todaysNutrient + ": " + Object.values(data)[currentRound-1][todaysNutrient] + " " + units[dt.getDay()];
 }
@@ -63,20 +69,22 @@ function addEventListeners() {
  * @param event - The event triggering the function
  */
 function updateCircle(event) {
-  let foodChoices = document.getElementsByClassName("game-img");
-  let circleComparison = document.getElementById("circle-comparison");
+  if (start === -1){
+    let foodChoices = document.getElementsByClassName("game-img");
+    let circleComparison = document.getElementsByClassName("circle")[0];
 
-  if (event.type == "mouseleave") {
-    circleComparison.textContent = "OR";
-  }
-  else if (event.type == "mouseenter" && event.target == foodChoices[0]) {
-    circleComparison.textContent = ">";
-  }
-  else if (event.type == "mouseenter" && event.target == foodChoices[1]) {
-    circleComparison.textContent = "<";
-  }
-  else {
-    throw ("updateCircle was called with an incorrect event!");
+    if (event.type == "mouseleave") {
+      circleComparison.textContent = "OR";
+    }
+    else if (event.type == "mouseenter" && event.target == foodChoices[0]) {
+      circleComparison.textContent = ">";
+    }
+    else if (event.type == "mouseenter" && event.target == foodChoices[1]) {
+      circleComparison.textContent = "<";
+    }
+    else {
+      throw ("updateCircle was called with an incorrect event!");
+    }
   }
 }
 
@@ -86,35 +94,99 @@ function updateCircle(event) {
  * @param event - The event triggering the function
  */
 function makeSelection(event) {
-  let foodChoices = document.getElementsByClassName("game-img");
+  if (start === -1){
+    let foodChoices = document.getElementsByClassName("game-img");
+    // Determine which food was selected
+    let foodSelected = event.target in foodChoices ? event.target : false;
+    if (foodChoices[0].contains(event.target) && !foodChoices[1].contains(event.target)) {
+      foodSelected = foodChoices[0];
+    }
+    else if (foodChoices[1].contains(event.target) && !foodChoices[0].contains(event.target)) {
+      foodSelected = foodChoices[1];
+    }
 
-  // Determine which food was selected
-  let foodSelected = event.target in foodChoices ? event.target : false;
-  if (foodChoices[0].contains(event.target) && !foodChoices[1].contains(event.target)) {
-    foodSelected = foodChoices[0];
-  }
-  else if (foodChoices[1].contains(event.target) && !foodChoices[0].contains(event.target)) {
-    foodSelected = foodChoices[1];
-  }
+    if (!foodSelected) {
+      throw ("Food selected covered both or neither of the image divs!");
+    }
 
-  if (!foodSelected) {
-    throw ("Food selected covered both or neither of the image divs!");
-  }
-
-  let roundDiv = document.getElementById('round-icons');
-  if (foodSelected == getMostNutritious() || !getMostNutritious()) {
-    roundDiv.childNodes[currentRound - 1].src = "static/images/" + nutrients[dt.getDay()].toLowerCase() + ".png";
-  }else{
-    roundDiv.childNodes[currentRound - 1].style.opacity = 0.5;
-  }
-  if (currentRound < rounds){
-    currentRound++;
-    generateFoodChoices();
-  }else{
-    //TODO: The game has ended, so do something here
-    alert("game over"); 
+    let roundDiv = document.getElementById('round-icons');
+    if (foodSelected == getMostNutritious() || !getMostNutritious()) {
+      roundDiv.childNodes[currentRound - 1].src = "static/images/" + nutrients[dt.getDay()].toLowerCase() + ".png";
+    }else{
+      roundDiv.childNodes[currentRound - 1].style.opacity = 0.5;
+    }
+    if (currentRound < rounds){
+      currentRound++;
+      animateFoods();
+    }else{
+      //TODO: The game has ended, so do something here
+      start = 0;
+    }
   }
 }
+
+function animateFoods(){
+  let foodDivs = document.getElementsByClassName("game-img");
+  let newFood = document.createElement("div");
+  let newCircle = document.createElement("div");
+  newFood.id = "nf";
+  //copy the second food item
+  newFood.innerHTML = foodDivs[1].innerHTML;
+  //but change the text and the image to be of the next food
+  newFood.firstElementChild.firstElementChild.innerHTML = Object.keys(data)[currentRound];
+  newFood.style.backgroundImage = "url(" + Object.values(data)[currentRound].url + ")";
+  newFood.className = "game-img";
+
+  newCircle.className = "circle";
+  newCircle.innerHTML = "OR";
+  document.body.appendChild(newFood);
+  document.getElementById("food-selection-area").appendChild(newCircle);
+  newFood.style.position = "absolute";
+  document.body.style.overflow = "hidden"; //Do this to avoid scrollbars appearing. Maybe put this in CSS instead
+  window.requestAnimationFrame(slide);
+}
+
+function slide(timestamp) {
+  let foodDivs = document.getElementsByClassName("game-img");
+  let slideSize = parseFloat(foodDivs[0].offsetWidth);
+  if (start === -1) {
+    start = timestamp;
+  }
+  let elapsed = timestamp - start;
+
+  if (previousTimeStamp !== timestamp) {
+    // Math.min() is used here to make sure the div stops at exactly the amount we want
+    let shift = Math.min(slideSize * Math.pow(elapsed,slidePower)/Math.pow(slideTime,slidePower), slideSize);
+    for (var i = 0; i < 2; i++){
+      foodDivs[i].style.transform = 'translateX(' + -shift + 'px)';
+      document.getElementsByClassName("circle")[i].style.left = 50*(slideSize*(i+1)-shift)/(slideSize) + "%";
+    }
+    //Need to keep calculating these in case the window size is changed
+    document.getElementById("nf").style.left =  2*slideSize-shift + "px";
+    document.getElementById("nf").style.top = foodDivs[0].getBoundingClientRect().top + "px";
+    document.getElementById("nf").style.width = slideSize + "px";
+    document.getElementById("nf").style.height = foodDivs[0].offsetHeight + "px";
+  }
+
+  if (elapsed < slideTime) {
+    previousTimeStamp = timestamp
+    window.requestAnimationFrame(slide);
+  }else{
+    resetAfterAnimation();
+  }
+}
+
+function resetAfterAnimation(){
+  document.body.removeChild(document.getElementById("nf"));
+  generateFoodChoices();
+  let foodDivs = document.getElementsByClassName("game-img");
+  for (var i = 0; i < 2; i++){
+    foodDivs[i].style.transform = 'translateX(0px)';
+  }
+  document.getElementById("food-selection-area").removeChild(document.getElementsByClassName("circle")[0]);
+  start = -1;
+}
+
 
 /**
  * Determines which choice is more nutritious. 
