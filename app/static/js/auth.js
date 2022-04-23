985551d9527d6a582e7e9a291357990009e51e04
ChantelleKerr <chantellekerr93@gmail.com>
2022-04-23 16:41:23 +0800

    User can now register an account through the frontend

--- app/static/js/auth.js
+++ app/static/js/auth.js
@@ -1,7 +1,30 @@
 const regoForm = document.getElementById("registrationForm");
 
+/* An event listener that waits for the registration form to be submitted
+ * which creates a HTTP POST Request with the form data
+ * 
+ * TODO: 
+ * Form Validation
+ * - Must be email and must be unique (Will return bad request if not unique )
+ * - Password must have certain length and numbers/characters?
+ * 
+ * Handle 201 status 
+ * - modal changes to login modal? 
+ * - and/or successful pop up message?
+ * Handle bad requests
+ */
 regoForm.addEventListener('submit', (event) => {
-  console.log(event);
-  // http request goes here - need database and user models set up
+  event.preventDefault();
 
+  const form = new FormData(event.target);
+  const user = form.get('username');
+  const e = form.get('email');
+  const pass = form.get('password');
+
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "http://localhost:5000/api/users/create", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.send(JSON.stringify({ username: user, email: e, password: pass }))
+
+  xhttp.onload = () => alert(xhttp.status)
 })
