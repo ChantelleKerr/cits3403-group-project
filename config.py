b73a68f07c3565d08cef542edaa3026c41f86e1f
ChantelleKerr <chantellekerr93@gmail.com>
2022-04-30 23:46:02 +0800

    Add test db to temp storage

--- config.py
+++ config.py
@@ -9,8 +9,7 @@ class Config(object):
 
 # Configuration class used for unittests
 class TestConfig(object):
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    'sqlite:///' + os.path.join(basedir, 'test.db')
+  SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   TESTING = True
 
