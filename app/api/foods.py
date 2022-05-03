951203c02b2b2297537b943eb93b83ac7158a881
Elodin77 <23061402@student.uwa.edu.au>
2022-05-03 11:05:13 +0800

    added get_daily_food (bugged)

---
+++ app/api/foods.py
@@ -0,0 +1,17 @@
+from app.api import bp
+from flask import jsonify, request, make_response, session, redirect
+from app import db
+from flask import url_for
+
+from datetime import datetime, timezone
+import json, random
+
+
+@bp.route('/foods', methods=['GET'])
+def get_daily_food():
+  seed = int(datetime.now(timezone.utc).strftime("%Y%m%d"))
+  with open("data.json") as file:
+    data = json.load(file) # data is dictionary containing every food
+  daily_foods = [data[x] for x in random.sample(data.keys(), 11)] # 11 is number of foods
+  return jsonify(daily_foods)
+
