b804aa012d63d16cd9441cbe9b22d798e4882bb8
Goldtiger997 <goldtiger997@BadWolf.localdomain>
2022-05-09 12:14:10 +0800

    move share_achievement() to results.py

--- app/api/results.py
+++ app/api/results.py
@@ -1,10 +1,12 @@
 from app.api import bp
 from app.models import Result
-from flask import jsonify, request, make_response, session, redirect
+from flask import jsonify, request, make_response
 from app import db
 from flask import url_for
 from flask_login import current_user, login_required
 import json
+import webbrowser
+import urllib.parse
 
 @bp.route('/results/<int:id>', methods=['GET'])
 def get_result(id):
@@ -35,3 +37,13 @@ def write_results():
   response.status_code = 201
   response.headers['Location'] = url_for('api.get_result', id=result.id)
   return response
+
+# Opens twitter and creates a tweet with given text (does not submit tweet automatically)
+@bp.route('/results/share', methods=['POST'])
+def share_achievement():
+  data = request.get_json() or {}
+  # Converts the text into this format -> %F0%9F%9F%A9
+  # Allows twitter to read the emojis correctly
+  encoded_data = (urllib.parse.quote_plus(data))
+  webbrowser.open_new_tab('http://twitter.com/intent/tweet?text=' + encoded_data)
+  return make_response(jsonify({"Success": "Share achievement Successful"}), 200)
