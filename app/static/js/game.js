fd1e266fd668f2047037f64e461285d8520489d1
Elodin77 <23061402@student.uwa.edu.au>
2022-05-18 16:41:07 +0800

    changes

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -284,7 +284,8 @@ function slide(timestamp) {
   }
   if (previousTimeStamp !== timestamp) {
     // Math.min() is used here to make sure the div stops at exactly the amount we want
-    let shift = Math.min(slideSize * Math.pow(elapsed, slidePower) / Math.pow(slideTime, slidePower), slideSize);
+    let x = elapsed/slideTime;
+    let shift = 1/(1+Math.pow(Math.E, -10*(x-0.5));
     for (let i = 0; i < foodDivs.length; i++) {
       foodDivs[i].style.transform = "translate" + slideDirection + "(" + -shift + "px)";
     }
