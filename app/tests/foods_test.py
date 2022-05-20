bf4b331ec50ef788cc31edeb21da37777b041727
Elodin77 <23061402@student.uwa.edu.au>
2022-05-20 21:48:08 +0800

    added tests for get_daily_foods

--- app/tests/foods_test.py
+++ app/tests/foods_test.py
@@ -14,6 +14,7 @@ class UserModelTest(unittest.TestCase):
   def tearDown(self):
     pass
 
+  # Tests the get_daily_nutrient function for the /api/foods/notd/<int:day> route
   def test_get_daily_nutrient(self):
     # Test the route with a zero parameter
     response = self.app.get('/api/foods/notd/0')
@@ -21,10 +22,10 @@ class UserModelTest(unittest.TestCase):
     self.assertEqual(200, response.status_code)
 
     self.assertEqual(dict, type(data))
-    self.assertEqual(2, len(data.keys()))
-
     self.assertEqual(str, type(data["notd"]))
     self.assertEqual(str, type(data["unit"]))
+
+    self.assertEqual(2, len(data.keys()))
 
     # Test the route with a non-zero parameter
     response = self.app.get('/api/foods/notd/1')
@@ -32,18 +33,32 @@ class UserModelTest(unittest.TestCase):
     self.assertEqual(200, response.status_code)
 
     self.assertEqual(dict, type(data))
-    self.assertEqual(2, len(data.keys()))
-
     self.assertEqual(list, type(data["nutrients"]))
     self.assertEqual(list, type(data["units"]))
+
+    self.assertEqual(2, len(data.keys()))
     self.assertEqual(7, len(data["nutrients"]))
     self.assertEqual(7, len(data["units"]))
     
-
+  # Tests the get_daily_foods function for the /api/foods/ route
   def test_get_daily_foods(self):
-    response = self.app.get('/api/foods/')
-
+    response = self.app.get("/api/foods/")
+    data = json.loads(response.data)
     self.assertEqual(200, response.status_code)
+
+    self.assertEqual(list, type(data))
+    self.assertEqual(11, len(data))
+
+    # Validate each food item in the returned data
+    for food in data:
+      for key in food.keys():
+        if key == "url":
+          self.assertTrue(food[key].startswith("https://"))
+        if key == "name" or key == "url":
+          self.assertTrue(type(food[key]) == str)
+        else:
+          self.assertTrue(type(food[key]) == int or type(food[key]) == float)
+
 
   def test_update_daily_foods(self):
     pass
